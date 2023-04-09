6bf8de70ecc67abb3dc74cd03404cebe104d7959
Agustin Mellado <aguxmellado@gmail.com>
2023-04-09 17:21:26 -0300

    inyeccion al constructor para obtener y trabajar con la id

--- src/app/pages/detalle-personaje/detalle-personaje.page.ts
+++ src/app/pages/detalle-personaje/detalle-personaje.page.ts
@@ -2,17 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
   selector: 'app-detalle-personaje',
   templateUrl: './detalle-personaje.page.html',
   styleUrls: ['./detalle-personaje.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,SharedModule]
 })
 export class DetallePersonajePage implements OnInit {
-
-  constructor() { }
+  personajeId: String= '';
+  constructor(private actRoute:ActivatedRoute) {
+    this.personajeId = this.actRoute.snapshot.paramMap.get('id') as String;
+   }
 
   ngOnInit() {
   }
