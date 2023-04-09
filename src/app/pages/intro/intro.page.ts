faf5ab708b622fd914dcbc2d40a85e3b9c6a1504
Agustin Mellado <aguxmellado@gmail.com>
2023-04-09 09:58:11 -0300

    creacion de estilos y funcion para redirigir a inicio

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -2,19 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class IntroPage implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    setTimeout(() => {
+      //Una vez que termine la animacion que navegue al home.
+      this.router.navigateByUrl('/inicio');
+      //cantidad de segundos
+    },1500);
   }
-
 }
