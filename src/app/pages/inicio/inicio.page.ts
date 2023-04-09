9a77b4f4bc89958a3cf1625dd9c4f433516495b5
Agustin Mellado <aguxmellado@gmail.com>
2023-04-09 12:37:36 -0300

    creacion de condicional para cargar y mostrar evento completado.

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
@@ -30,8 +30,13 @@ export class InicioPage implements OnInit {
         //espero la respuesta de la api.
         this.characters.push(...res.results); //como es arreglo y necesito sacar todos, aplico spread Operator
         console.log(this.characters);
+        //Si existe un evento
+        if (event) event.target.complete(); //que lo marque como evento completado, para mostrar los personajes
       },
-      error: (error: any) => {},
+      error: (error: any) => {
+        //Si existe un evento
+        if (event) event.target.complete(); //que lo marque como evento completado, para mostrar los personajes
+      },
     });
   }
 }
